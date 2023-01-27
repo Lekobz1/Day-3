3b707ea3a945961c203be89881abe66735076606
Lekobz1 <jonathan.simon.pro1@gmail.com>
2023-01-27 11:28:45 +0100

    messqgae

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = "toto";
+let lastName = "asticot";
+let country = "france";
+let city = "paris";
+let age = 22;
+let isMarried = false;
+let year = 2023;
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
+
+console.log("10" == 10);
+
+console.log(parseInt("9.98") == 10);
+
+console.log(12/12 == 1);
+console.log(3 * 3 == 9);
+console.log("green" == "green");
+
+const now = new Date();
+console.log(now.getFullYear());
